a0303d6bc1466894ac364b88e1ab25c5e10cdc41
K.J. Chen <kjchen@pm.me>
2019-04-04 21:45:55 +0800

    Fixed BUG

--- PiConsole/settings.py
+++ PiConsole/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nfp##h=q0f474vtdwi1=n^z90dw$35^*yo&6dq+rc!4p@7=-hs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.43.158"]
+ALLOWED_HOSTS = ['192.168.43.158']
 
 
 # Application definition
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-tw'
 
 TIME_ZONE = 'Asia/Taipei'
 
