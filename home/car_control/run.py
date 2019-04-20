4694b0ca0bbaac7ce27e335395bc2c32734f5d64
K.J. Chen <kjchen@pm.me>
2019-04-20 10:33:13 +0800

    Add Method Called "infrared_avoid"

--- home/car_control/run.py
+++ home/car_control/run.py
@@ -12,7 +12,14 @@ class CarRun:
         EN_A = 16
         global EN_B
         EN_B = 13
-        #global dt = 3
+        """
+        global key
+        key = 8
+        """
+        global AvoidSensorLeft
+        AvoidSensorLeft = 12
+        global AvoidSensorRight
+        AvoidSensorRight = 17
 
     #def __new__(self, dt = 1):
         GPIO.setmode(GPIO.BCM)
@@ -36,6 +43,10 @@ class CarRun:
         GPIO.setup(EN_B, GPIO.OUT, initial = 1)
         pwm_EN_B = GPIO.PWM(EN_B, 2000)
         pwm_EN_B.start(0)
+
+        #GPIO.setup(key,GPIO.IN)
+        GPIO.setup(AvoidSensorLeft,GPIO.IN)
+        GPIO.setup(AvoidSensorRight,GPIO.IN)
 
     @staticmethod
     def brake(dt = 1):
@@ -61,7 +72,8 @@ class CarRun:
         time.sleep(self.dt)
         if b_k == 0:
             self.brake()
-        self.request = request
+        return redirect("javascript:window.close();")
+        #self.request = request
 
     # Go Back
     def back(self, request, b_k = 0):
@@ -76,7 +88,8 @@ class CarRun:
         time.sleep(self.dt)
         if b_k == 0:
             self.brake()
-        self.request = request
+        return redirect("javascript:window.close();")
+        #self.request = request
 
     # Turn Left
     def left(self, request, b_k = 0):
@@ -90,7 +103,8 @@ class CarRun:
         time.sleep(self.dt)
         if b_k == 0:
             self.brake()
-        self.request = request
+        return redirect("javascript:window.close();")
+        #self.request = request
 
     # Turn Right
     def right(self, request, b_k = 0):
@@ -104,7 +118,8 @@ class CarRun:
         time.sleep(self.dt)
         if b_k == 0:
             self.brake()
-        self.request = request
+        return redirect("javascript:window.close();")
+        #self.request = request
 
     # Turn Left in Situd
     def situ_left(self, request, b_k = 0):
@@ -118,7 +133,8 @@ class CarRun:
         time.sleep(self.dt)
         if b_k == 0:
             self.brake()
-        self.request = request
+        return redirect("javascript:window.close();")
+        #self.request = request
 
     # Turn Right in Situ
     def situ_right(self, request, b_k = 0):
@@ -132,12 +148,31 @@ class CarRun:
         time.sleep(self.dt)
         if b_k == 0:
             self.brake()
-        self.request = request
+        return redirect("javascript:window.close();")
+        #self.request = request
+
+    # Infrared Avoid
+    def infrared_avoid(self, request):
+        i = None
+        while i == None:
+            LeftSensorValue  = GPIO.input(AvoidSensorLeft)
+            RightSensorValue = GPIO.input(AvoidSensorRight)
+
+            if LeftSensorValue == True and RightSensorValue == True:
+                run()
+            elif LeftSensorValue == False and RightSensorValue == True:
+                spin_right()
+            time.sleep(0.002)
+            elif LeftSensorValue == True and RightSensorValue == False:
+                spin_left()
+            time.sleep(0.002)
+        return redirect("javascript:window.close();")
 
     # Stop to Run the Car
     def stop_run(self, request):
         self.brake()
-        self.request = request
+        return redirect("javascript:window.close();")
+        #self.request = request
 
     # End Control
     def all_stop(self, request):
@@ -146,3 +181,4 @@ class CarRun:
         pwm_EN_A.stop()
 
         GPIO.cleanup()
+        return redirect("javascript:window.close();")
